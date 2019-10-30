59fdb044124d7ea51964651f2984529235350922
manjusha <amanjushait@gmail.com>
2019-10-30 07:59:29 +0530

    my new project

---
+++ manju/src/main/java/com/varsha/manju/App.java
@@ -0,0 +1,13 @@
+package com.varsha.manju;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ manju/src/main/java/com/varsha/manju/ExtractAndSumFromString.java
@@ -0,0 +1,38 @@
+package com.varsha.manju;
+
+public class ExtractAndSumFromString {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+     int a = getSumNumbers("abc 4 test 5 notext 5 ttt  6");
+     
+     System.out.println(a);
+	}
+	
+	public static int getSumNumbers(String s){
+		
+		int total = 0;
+		//split the string by space
+		String[] input = s.split("\\s+"); 
+		
+		for(String s1 : input){
+			int nextNumber = 0;
+			
+			try{
+				nextNumber = Integer.parseInt(s1);
+			}
+			catch (NumberFormatException e) {
+				// TODO: handle exception
+				System.out.println("not a number");
+			}
+			
+			finally {
+				total += nextNumber;
+			}
+		}
+			
+		return total;
+	}
+	
+
+}
